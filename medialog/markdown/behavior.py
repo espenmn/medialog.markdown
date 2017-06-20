d88d61689500bea0972cf531b589802f0509673b
Espen Moe-Nilssen <espen@medialog.no>
2017-06-20 22:33:10 +0200

    trying with richtext

--- medialog/markdown/behavior.py
+++ medialog/markdown/behavior.py
@@ -1,5 +1,6 @@
 from zope import schema
 from zope.interface import Interface
+from plone.app.textfield import RichText
 from zope.interface import implements
 from plone.directives import form
 from plone.autoform.interfaces import IFormFieldProvider
@@ -20,9 +21,18 @@ class IMarkdownBehavior(form.Schema):
         description = _("help_markdown",
                       default="Markdown text"),
     )
+    
+    bodyText = RichText(
+        title=u"Body text",
+        default_mime_type='text/x-web-markdown',
+        output_mime_type='text/html',
+        allowed_mime_types=('text/x-web-markdown',),
+        default=u"Default value"
+    )
 
     form.widget(
         markdown=MarkdownFieldWidget,
+        bodyText=MarkdownFieldWidget,
     )
 
 alsoProvides(IMarkdownBehavior, IFormFieldProvider)
