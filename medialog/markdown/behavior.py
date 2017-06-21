67a44454184ca31455e25266e3396c356075b637
Espen Moe-Nilssen <espen@medialog.no>
2017-06-21 15:21:05 +0200

    working, stripped version

--- medialog/markdown/behavior.py
+++ medialog/markdown/behavior.py
@@ -1,6 +1,6 @@
 from zope import schema
 from zope.interface import Interface
-from plone.app.textfield import RichText
+#from plone.app.textfield import RichText
 from zope.interface import implements
 from plone.directives import form
 from plone.autoform.interfaces import IFormFieldProvider
@@ -10,8 +10,8 @@ from zope.i18nmessageid import MessageFactory
 from z3c.form.browser.textarea import TextAreaFieldWidget
 
 #do I need this?
-from plone.app.textfield.interfaces import IRichTextValue
-from plone.app.textfield.interfaces import ITransformer
+#from plone.app.textfield.interfaces import IRichTextValue
+#from plone.app.textfield.interfaces import ITransformer
 
 
 from medialog.markdown.widgets.widget import MarkdownFieldWidget
@@ -23,23 +23,15 @@ class IMarkdownBehavior(form.Schema):
     """ A markdown text field"""
     
     markdown = schema.Text(
-        title = _("markdown", default=u"Markdown"),
+        title = _("markdown", default=u"Text"),
         required = False,
         description = _("help_markdown",
                       default="Markdown text"),
     )
   
-    bodyText = RichText(
-        title=u"Body text",
-        default_mime_type='text/x-web-markdown',
-        output_mime_type='text/html',
-        allowed_mime_types=('text/x-web-markdown',),
-        default=u"Default value"
-    )
   
     form.widget(
           markdown=MarkdownFieldWidget,
-          bodyText=MarkdownFieldWidget,
     )
           
 alsoProvides(IMarkdownBehavior, IFormFieldProvider)
