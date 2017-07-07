577294a8d3df0468f0a7efd6fc66db45c88cbfe6
Espen Moe-Nilssen <espen@medialog.no>
2017-07-07 11:26:52 +0200

    Update tile.py

--- medialog/markdown/tiles/tile.py
+++ medialog/markdown/tiles/tile.py
@@ -1,22 +1,41 @@
 # -*- coding: utf-8 -*-
 
- 
+#from plone import api
+
 from plone.memoize.view import memoize
 from plone.supermodel import model
-from plone.tiles import PersistentTile
-from plone.tiles.interfaces import ITileType
+#from plone.tiles import Tile
 from zope import schema
-from zope.component import getUtility
+from plone.directives import form
 from zope.i18nmessageid import MessageFactory
+from plone.tiles import Tile
 from zope.interface import provider
+#from collective import dexteritytextindexer
 from zope.schema import getFields
 
-    
-class IMarkdownTile(model.Schema):
-    
-    body = schema.Text(title=u"Rich text")
-    
-    
-class MarkdownTile(PersistentTile):
-    """A tile that displays image and richtext"""
 
+from medialog.markdown.interfaces import IMarkdownSettings
+from medialog.markdown.widgets.widget import MarkdownFieldWidget
+_ = MessageFactory('medialog.markdownn')
+
+
+#ITile.implementedBy(MarkdownTile)
+
+class IMarkdownTile(model.Schema):
+
+    #dexteritytextindexer.searchable('bodytext')
+
+    bodytext = schema.Text(
+    	title=u"Body text",
+    )
+
+    form.widget(
+          bodytext=MarkdownFieldWidget,
+    )
+
+
+class MarkdownTile(Tile):
+    """A tile that displays markdown text"""
+
+    def __init__(self, context, request):
+        super(MarkdownTile, self).__init__(context, request)
